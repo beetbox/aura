f9c87981a8bcaa0c5502d072add9762d32aa3fc4
Adrian Sampson <adrian@radbox.org>
2014-10-26 22:36:00 -0700

    Proof-of-concept reference implementation

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ AURA Universal REST API
 
 AURA is a design for simple, REST API for music libraries. It's the glue between music players and libraries of music.
 
-Take a look at [the evolving spec](http://auraspec.readthedocs.org/).
+Take a look at [the evolving spec](http://auraspec.readthedocs.org/). The ``ref`` directory contains a reference implementation of the spec written in JavaScript.
---
+++ ref/auraserver.js
@@ -0,0 +1,69 @@
+var fs = require('fs');
+var musicmetadata = require('musicmetadata');
+var walk = require('walk');
+var path = require('path');
+var express = require('express');
+
+// Read a track dictionary for a music file.
+var trackDict = function(path, func) {
+  var parser = musicmetadata(fs.createReadStream(path));
+  parser.on('metadata', function (metadata) {
+    func({
+      'path': path,
+      'artist': metadata.artist[0],
+      'title': metadata.title,
+      'album': metadata.album,
+    });
+  });
+};
+
+// Read all the music files in a directory (recursively) and record all of
+// their metadata dictionaries in a giant array.
+var readMetadata = function (basedir, func) {
+  var md = [];
+  var walker = walk.walk(basedir);
+  walker.on("file", function (root, stats, next) {
+    var p = path.join(root, stats.name);
+    console.log('read: ' + p);
+    trackDict(p, function (d) {
+      md.push(d);
+    });
+    next();
+  }).on("end", function () {
+    // Add IDs to each.
+    for (var i = 0; i < md.length; ++i) {
+      md[i].id = i.toString();
+    }
+    func(md);
+  });
+};
+
+// Read the metadata and start the server.
+readMetadata(process.argv[2] || '.', function (tracks) {
+  var loadTrack = function (req, res, next) {
+    var id = parseInt(req.params.id);
+    if (isNaN(id) || id < 0 || id > tracks.length) {
+      res.status(404).end();
+    } else {
+      req.track = tracks[id];
+      next();
+    }
+  };
+
+  var app = express();
+  app.get('/tracks', function (req, res) {
+    res.json({ 'tracks': tracks });
+  });
+  app.get('/tracks/:id', loadTrack, function (req, res) {
+    res.json({ 'tracks': [ req.track ] });
+  });
+  app.get('/tracks/:id/audio', loadTrack, function (req, res) {
+    res.sendFile(req.track.path);
+  });
+
+  var server = app.listen(8338, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('http://%s:%s', host, port);
+  });
+});
