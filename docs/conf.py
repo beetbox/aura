32c7169e4735b3c06904f9de070e32752b5bf43f
Adrian Sampson <adrian@radbox.org>
2014-10-26 17:28:28 -0700

    Sphinx skeleton

---
+++ docs/conf.py
@@ -0,0 +1,11 @@
+source_suffix = '.rst'
+master_doc = 'index'
+exclude_patterns = ['_build']
+
+project = u'Urma'
+copyright = u'2014, Adrian Sampson'
+
+version = '0.1'
+release = '0.1.0'
+
+html_theme = 'default'
---
+++ docs/index.rst
@@ -0,0 +1,7 @@
+Urma: the Universal REST Music API
+==================================
+
+Nothing to see yet! Contents:
+
+.. toctree::
+   :maxdepth: 2
